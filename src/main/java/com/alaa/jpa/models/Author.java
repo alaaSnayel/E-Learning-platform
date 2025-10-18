189a019bde8167b893c2be5a00b1b4b131c4003d
alaaSnayel <alaanayel111@gmail.com>
2025-10-18 15:27:32 +0300

    TableGenerator annotation

--- src/main/java/com/alaa/jpa/models/Author.java
+++ src/main/java/com/alaa/jpa/models/Author.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.TableGenerator;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,12 +15,16 @@ import lombok.NoArgsConstructor;
 public class Author {
   @Id
   @GeneratedValue(
-    strategy = GenerationType.SEQUENCE,
-    generator = "author_sequence"
+    strategy = GenerationType.TABLE,
+    generator = "author_id_generator"
   )
-  @SequenceGenerator(
-    name = "author_sequence",
-    sequenceName = "author_sequence",
+  @TableGenerator(
+    name = "author_id_generator",
+    table = "id_gen_table",
+    pkColumnName = "id_name",
+    valueColumnName = "gen_value",
+    pkColumnValue = "author_id",
+    initialValue = 1000,
     allocationSize = 1
   )
   private Integer id;
