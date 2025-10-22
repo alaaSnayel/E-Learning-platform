df9e87ef395decf7a1d556519d2e8061e0c0535a
alaaSnayel <alaanayel111@gmail.com>
2025-10-22 14:31:43 +0300

    One to many between section and lecture

--- src/main/java/com/alaa/jpa/models/Section.java
+++ src/main/java/com/alaa/jpa/models/Section.java
@@ -1,10 +1,13 @@
 package com.alaa.jpa.models;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -27,4 +30,7 @@ public class Section {
   @ManyToOne
   @JoinColumn(name = "course_id")
   private Course course;
+
+  @OneToMany(mappedBy = "section")
+  private List<Lecture> lectures;
 }
