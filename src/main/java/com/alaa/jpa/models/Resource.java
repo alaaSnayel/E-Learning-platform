ed5464c647189771ba39b13836a7bcf6ed53ef05
alaaSnayel <alaanayel111@gmail.com>
2025-10-22 14:49:20 +0300

    bi-direction OneToOne

--- src/main/java/com/alaa/jpa/models/Resource.java
+++ src/main/java/com/alaa/jpa/models/Resource.java
@@ -3,6 +3,7 @@ package com.alaa.jpa.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,4 +25,8 @@ public class Resource {
   private int size;
 
   private String url;
+
+  @OneToOne
+  @JoinColumn(name = "lecture_id")
+  private Lecture lecture;
 }
